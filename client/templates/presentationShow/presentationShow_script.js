f9022a6f43e5cfbccdf8b9721e5034b0aa13cf74
Mário Balça <mario.balca@gmail.com>
2016-02-21 14:00:53 +0000

    fixed show deck - desktop version

--- client/templates/presentationShow/presentationShow_script.js
+++ client/templates/presentationShow/presentationShow_script.js
@@ -2,11 +2,21 @@ Template.presentationShow.helpers({
 	relativeUrl: function(param) {
 		if (param == 'question')
 			return '/question/' + Router.current().params._id;
+	},
+	slides: function() {
+		return Slides.find({presentation: {id: Router.current().params._id}});
+	},
+	current_layout: function(param) {
+		return Slides.findOne({_id: Session.get('activeSlideId')}).layout.name == param;
 	}
 });
 
 Template.presentationShow.events({
 });
+
+Template.presentationEdit.onCreated(function (){
+	Session.set('activeSlideId', Slides.findOne({presentation: {id: Router.current().params._id}})._id);
+})
 
 Template.presentationShow.onRendered(function ( ){
 	 $('ul.tabs').tabs();
