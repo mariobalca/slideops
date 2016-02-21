64641f2f0351781167a146198bccf20595576d77
Mário Balça <mario.balca@gmail.com>
2016-02-21 15:40:25 +0000

    initilazed questions

--- collections/questions.js
+++ collections/questions.js
@@ -1,5 +1,39 @@
 Questions = new Mongo.Collection("questions");
 
-QuestionsPresentationSchema = new SimpleSchema({
-    id: {type: String}
+QuestionsSchema = new SimpleSchema({
+    id: {type: String},
+    data: {type: Object},
+    answers: {type: Oject}
+});
+
+
+Questions.allow({
+    insert: function(userId, doc){
+        return true;
+    },
+    /*update: function(){
+        return true;
+    },*/
+    remove: function(userId, doc){
+        return true;
+    }
+});
+
+Meteor.methods({
+    createQuestion: function(slide_id, data){
+        check(Meteor.userId(), String);
+
+        question = {
+            data: data
+        }
+
+        var id = Questions.insert(question);
+        return id;
+    },
+    updateData: function(data){
+        Questions.update({_id: p}, {$set: {data: data}});
+    },
+    anwswer: function(data, ) {
+
+    }
 });
