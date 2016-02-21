88dc709cc0957ae9389338f2aa4d54e3ec54cc15
Mário Balça <mario.balca@gmail.com>
2016-02-21 16:22:00 +0000

    envio

--- client/templates/presentationShow/presentationShow_script.js
+++ client/templates/presentationShow/presentationShow_script.js
@@ -7,7 +7,9 @@ Template.presentationShow.helpers({
 		return Slides.find({presentation: {id: Router.current().params._id}});
 	},
 	current_layout: function(param) {
-		return Slides.findOne({_id: Session.get('activeSlideId')}).layout.name == param;
+		pres = Presentations.findOne({_id: Router.current().params._id});
+		slide = Slides.findOne({_id: pres.slides[pres.live.current_slide-1]});
+		return slide.layout.name == param;
 	}
 });
 
@@ -15,7 +17,7 @@ Template.presentationShow.events({
 });
 
 Template.presentationEdit.onCreated(function (){
-	Session.set('activeSlideId', Slides.findOne({presentation: {id: Router.current().params._id}})._id);
+
 })
 
 Template.presentationShow.onRendered(function ( ){
